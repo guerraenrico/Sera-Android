29fbcf61211b92c7f4f5d3269f704a73fc405f7e
guerraenrico <portapozze@libero.it>
2020-02-18 22:23:15 +0100

    Make typealias private, and add new compose

--- base/src/main/java/com/guerra/enrico/base/extensions/FunctionExtension.kt
+++ base/src/main/java/com/guerra/enrico/base/extensions/FunctionExtension.kt
@@ -5,11 +5,11 @@ package com.guerra.enrico.base.extensions
  * on 18/02/2020.
  */
 
-typealias F1<T> = (T) -> T
+private typealias F1<T> = (T) -> T
 
-typealias F2<T, A> = (T, A) -> T
+private typealias F2<T, A> = (T, A) -> T
 
-typealias F3<T, A, B> = (T, A, B) -> T
+private typealias F3<T, A, B> = (T, A, B) -> T
 
 infix fun <T> F1<T>.compose(f1: F1<T>): (T) -> T {
   return { t: T -> f1(this(t)) }
@@ -17,6 +17,10 @@ infix fun <T> F1<T>.compose(f1: F1<T>): (T) -> T {
 
 infix fun <T, A> F2<T, A>.compose(f1: F1<T>): (T, A) -> T {
   return { t: T, a: A -> f1(this(t, a)) }
+}
+
+infix fun <T, A, B> F2<T, A>.compose(f2: F2<T, B>): (T, A, B) -> T {
+  return { t: T, a: A, b: B -> f2(this(t, a), b) }
 }
 
 infix fun <T, A, B> F3<T, A, B>.compose(f1: F1<T>): (T, A, B) -> T {
