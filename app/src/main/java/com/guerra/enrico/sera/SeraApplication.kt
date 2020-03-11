5fcda957306905aacad03330066aabca81606b8b
guerraenrico <portapozze@libero.it>
2020-03-11 22:40:54 +0100

    Add flipper

--- app/src/main/java/com/guerra/enrico/sera/SeraApplication.kt
+++ app/src/main/java/com/guerra/enrico/sera/SeraApplication.kt
@@ -1,7 +1,14 @@
 package com.guerra.enrico.sera
 
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.work.Configuration
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.soloader.SoLoader
+import com.guerra.enrico.base.PreferencesFile
 import com.guerra.enrico.sera.appinitializers.AppInitializers
 import com.guerra.enrico.sera.di.component.DaggerAppComponent
 import dagger.android.AndroidInjector
@@ -17,8 +24,13 @@ import javax.inject.Inject
 class SeraApplication : DaggerApplication(), Configuration.Provider {
   @Inject
   lateinit var initializers: AppInitializers
+
   @Inject
   lateinit var workConfiguration: Configuration
+
+  @Inject
+  @PreferencesFile
+  lateinit var preferenceFile: String
 
   override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
     return DaggerAppComponent.factory().create(this)
@@ -30,6 +42,15 @@ class SeraApplication : DaggerApplication(), Configuration.Provider {
 
     if (BuildConfig.DEBUG) {
       Timber.plant(Timber.DebugTree())
+    }
+
+    SoLoader.init(this, false)
+    if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+      val client = AndroidFlipperClient.getInstance(this)
+      client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+      client.addPlugin(SharedPreferencesFlipperPlugin(this, preferenceFile))
+      client.addPlugin(DatabasesFlipperPlugin(this));
+      client.start()
     }
 
     LeakCanary.config = LeakCanary.config.copy(dumpHeap = false)
