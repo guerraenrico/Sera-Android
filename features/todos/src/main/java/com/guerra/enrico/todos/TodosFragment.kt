4fd89260e165da162983e5454e48459e9f5d900e
guerraenrico <portapozze@libero.it>
2020-05-08 23:32:41 +0200

    WIP modularization: created firsts modules, start moving things

---
+++ features/todos/src/main/java/com/guerra/enrico/todos/TodosFragment.kt
@@ -0,0 +1,8 @@
+package com.guerra.enrico.todos
+
+/**
+ * Created by enrico
+ * on 08/05/2020.
+ */
+class TodosFragment {
+}
---
+++ features/todos/src/main/java/com/guerra/enrico/todos/TodosViewModel.kt
@@ -0,0 +1,6 @@
+package com.guerra.enrico.todos
+
+/**
+ * Created by enrico
+ * on 08/05/2020.
+ */
