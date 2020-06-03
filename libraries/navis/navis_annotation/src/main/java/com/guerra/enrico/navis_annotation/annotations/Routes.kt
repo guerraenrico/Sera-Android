849586eb8e6d98ab066dbf99c0849bec004a6bc2
guerraenrico <portapozze@libero.it>
2020-06-03 22:23:01 +0200

    WIP navigation: change annotation retention

--- libraries/navis/navis_annotation/src/main/java/com/guerra/enrico/navis_annotation/annotations/Routes.kt
+++ libraries/navis/navis_annotation/src/main/java/com/guerra/enrico/navis_annotation/annotations/Routes.kt
@@ -4,6 +4,6 @@ package com.guerra.enrico.navis_annotation.annotations
  * Created by enrico
  * on 02/06/2020.
  */
-@Retention(AnnotationRetention.RUNTIME)
+@Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.CLASS)
 annotation class Routes
