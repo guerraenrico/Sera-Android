1e963bc927fba3c414d4b0cf5c0229f9f4f49d4d
guerraenrico <portapozze@libero.it>
2020-06-22 23:28:32 +0200

    WIP navigation: remove portum annotation (for now)

--- libraries/navis/navis_annotation/src/main/java/com/guerra/enrico/navis_annotation/annotations/Portum.kt
+++
@@ -1,9 +0,0 @@
-package com.guerra.enrico.navis_annotation.annotations
-
-/**
- * Created by enrico
- * on 02/06/2020.
- */
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.CLASS)
-annotation class Portum
