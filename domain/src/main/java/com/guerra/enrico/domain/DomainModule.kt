fa84bc1ca2fec330783bb1b0d5b72de7f6d50d9c
guerraenrico <portapozze@libero.it>
2020-07-04 23:11:11 +0200

    Hilt migration add workerModule to inject workerManager

--- domain/src/main/java/com/guerra/enrico/domain/DomainModule.kt
+++ domain/src/main/java/com/guerra/enrico/domain/DomainModule.kt
@@ -1,7 +1,11 @@
 package com.guerra.enrico.domain
 
+import com.guerra.enrico.domain.interactors.todos.ValidateToken
+import com.guerra.enrico.domain.observers.todos.ObserveCategories
 import com.guerra.enrico.sera.data.repo.DataModule
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 
@@ -9,6 +13,7 @@ import dagger.hilt.android.components.ApplicationComponent
  * Created by enrico
  * on 01/04/2020.
  */
-@InstallIn(ApplicationComponent::class)
-@Module(includes = [DataModule::class])
-class DomainModule
+//@InstallIn(ApplicationComponent::class)
+//@Module(includes = [DataModule::class])
+//abstract class DomainModule {
+//}
---
+++ workers/src/main/java/com/guerra/enrico/workers/di/WorkerModule.kt
@@ -0,0 +1,22 @@
+package com.guerra.enrico.workers.di
+
+import android.content.Context
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+/**
+ * Created by enrico
+ * on 04/07/2020.
+ */
+@InstallIn(ApplicationComponent::class)
+@Module(includes = [WorkersModuleBinds::class])
+object WorkerModule {
+  @Provides
+  fun provideWorkManager(@ApplicationContext context: Context): WorkManager {
+    return WorkManager.getInstance(context)
+  }
+}
