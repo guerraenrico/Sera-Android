c3a27d58e9e5d731cae1674f8d007cad93b3c25b
guerraenrico <portapozze@libero.it>
2020-08-01 23:58:13 +0200

    remove login and splash activities, create main fragment to hold the bottom navigation graph

--- app/src/main/java/com/guerra/enrico/sera/main/MainActivity.kt
+++ app/src/main/java/com/guerra/enrico/sera/main/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.guerra.enrico.sera.main
 
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.guerra.enrico.base.extensions.setLightStatusBarIfNeeded
 import com.guerra.enrico.base.extensions.systemUiFullScreen
 import com.guerra.enrico.base_android.arch.BaseActivity
@@ -18,25 +15,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
 
-  private val viewModel: MainViewModel by viewModels()
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setLightStatusBarIfNeeded()
 
     setContentView(R.layout.activity_main)
 
-    root.systemUiFullScreen()
-
-    setupBottomNavigationBar()
+    rootHost.systemUiFullScreen()
   }
 
-  private fun setupBottomNavigationBar() {
-    bottomNavigation.setupWithNavController(findNavController(R.id.mainFragmentHost))
-    bottomNavigation.setOnNavigationItemSelectedListener { item ->
-      viewModel.onSelectMenuItem(item.itemId)
-      true
-    }
-    bottomNavigation.setOnNavigationItemReselectedListener { /* Disable default behavior */ }
-  }
 }
--- navigation/src/main/java/com/guerra/enrico/navigation/Navigator.kt
+++ navigation/src/main/java/com/guerra/enrico/navigation/Navigator.kt
@@ -8,6 +8,7 @@ import android.net.Uri
  */
 interface Navigator {
   fun getUriLogin(): Uri
+  fun getUriMain(): Uri
   fun getUriTodos(): Uri
   fun getUriGoals(): Uri
   fun getUriResults(): Uri
