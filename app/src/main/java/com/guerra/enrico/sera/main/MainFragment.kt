36b3a997fb4a3d7d5eb87e99b5421b3ce6c1de61
guerraenrico <portapozze@libero.it>
2020-08-02 11:48:49 +0200

    fix bottom navigation: get use child nav host

--- app/src/main/java/com/guerra/enrico/sera/main/MainFragment.kt
+++ app/src/main/java/com/guerra/enrico/sera/main/MainFragment.kt
@@ -3,7 +3,8 @@ package com.guerra.enrico.sera.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.guerra.enrico.sera.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,18 +15,18 @@ import kotlinx.android.synthetic.main.fragment_main.*
  * on 01/08/2020.
  */
 @AndroidEntryPoint
-class MainFragment: Fragment(R.layout.fragment_main) {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    setupBottomNavigationBar()
+    val nestedHostFragment: NavHostFragment =
+      childFragmentManager.findFragmentById(R.id.mainFragmentHost) as NavHostFragment
+    val navController = nestedHostFragment.navController
+    setupBottomNavigationBar(navController)
   }
 
-  private fun setupBottomNavigationBar() {
-    bottomNavigation.setupWithNavController(findNavController())
-    bottomNavigation.setOnNavigationItemSelectedListener {
-      true
-    }
+  private fun setupBottomNavigationBar(navController: NavController) {
+    bottomNavigation.setupWithNavController(navController)
     bottomNavigation.setOnNavigationItemReselectedListener { /* Disable default behavior */ }
   }
 
