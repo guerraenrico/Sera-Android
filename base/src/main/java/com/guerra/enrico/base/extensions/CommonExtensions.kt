7cad80fd1785c1d1c83ddb30021774ad7d2a6a85
guerraenrico <portapozze@libero.it>
2020-08-08 23:25:48 +0200

    refactor goals

--- base/src/main/java/com/guerra/enrico/base/extensions/CommonExtensions.kt
+++ base/src/main/java/com/guerra/enrico/base/extensions/CommonExtensions.kt
@@ -3,11 +3,6 @@ package com.guerra.enrico.base.extensions
 import java.text.SimpleDateFormat
 import java.util.*
 
-/**
- * Created by enrico
- * on 02/06/2018.
- */
-
 fun Date.toDateString(pattern: String = "EEEE dd MMM yyyy"): String {
   val simpleDateFormat = SimpleDateFormat(pattern, Locale.US)
   return simpleDateFormat.format(this)
--- features/goals/src/main/java/com/guerra/enrico/goals/GoalsFragment.kt
+++ features/goals/src/main/java/com/guerra/enrico/goals/GoalsFragment.kt
@@ -1,33 +1,19 @@
 package com.guerra.enrico.goals
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.material.transition.MaterialFadeThrough
-import com.guerra.enrico.base_android.extensions.applyWindowInsets
 import com.guerra.enrico.base_android.arch.BaseFragment
 import com.guerra.enrico.base_android.exception.MessageExceptionManager
+import com.guerra.enrico.base_android.extensions.applyWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_goals.*
 
-/**
- * Created by enrico
- * on 27/05/2018.
- */
 @AndroidEntryPoint
-internal class GoalsFragment : BaseFragment() {
+internal class GoalsFragment : BaseFragment(R.layout.fragment_goals) {
 
   private val viewModel: GoalsViewModel by viewModels()
-
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_goals, container, false)
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
