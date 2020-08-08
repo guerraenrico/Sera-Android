b7e995811d563ab9f83f9eaec9dee8b513549295
guerraenrico <portapozze@libero.it>
2020-08-08 23:27:02 +0200

    refactor results

--- features/results/src/main/java/com/guerra/enrico/results/ResultsFragment.kt
+++ features/results/src/main/java/com/guerra/enrico/results/ResultsFragment.kt
@@ -1,32 +1,18 @@
 package com.guerra.enrico.results
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.material.transition.MaterialFadeThrough
-import com.guerra.enrico.base_android.extensions.applyWindowInsets
 import com.guerra.enrico.base_android.arch.BaseFragment
 import com.guerra.enrico.base_android.exception.MessageExceptionManager
+import com.guerra.enrico.base_android.extensions.applyWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_results.*
 
-/**
- * Created by enrico
- * on 30/07/2019.
- */
 @AndroidEntryPoint
-internal class ResultsFragment : BaseFragment() {
+internal class ResultsFragment : BaseFragment(R.layout.fragment_results) {
   private val viewModel: ResultsViewModel by viewModels()
-
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_results, container, false)
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
